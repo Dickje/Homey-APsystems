2f5e6c36dace63dc752c7be9d8dd53443c0d99b7
Dickje <86379084+Dickje@users.noreply.github.com>
2025-05-24 17:48:42 +0200

    Update driver.js

--- drivers/solarpanels/driver.js
+++ drivers/solarpanels/driver.js
@@ -2,68 +2,55 @@
 
 const Homey = require('homey');
 const MyApi = require('./api');
-const sid = Homey.env.CLIENT_SID;
 
 module.exports = class MyDriver extends Homey.Driver {
 
-  async onInit() {
-    this.log('MyDriver has been initialized');
-  }
-  async onPair(session) {
-    session.setHandler("keys_entered", async (data) => {
-      this.homey.log("Pair data received");
+  async onInit() {this.log('MyDriver has been initialized');}
 
-      const { apiKey, apiSecret } = data;
+  async onPair(session) {
+    let devices='';
+    
+      session.setHandler("keys_entered", async (data) =>
+      {
+      this.log("Api credentials received");
+
+      // Get the entered keys
+      const { apiKey, apiSecret, sid } = data;
       this.homey.settings.set("apiKey", apiKey);
       this.homey.settings.set("apiSecret", apiSecret);
-      console.log("onpair");
-      console.log(apiKey);
-      console.log(apiSecret);
-    this.log('Pairing...')
-    const pairApi = new MyApi;
-    
-    const returndata = await pairApi.fetchData('/user/api/v2/systems/details/' + sid,'', 'GET')
-    const device = [
-           { 
-        name: 'APsystems',
-        data : {sid_id: returndata.data.sid},
-        store: {
-                installdate: returndata.data.create_date,
-                capacity: returndata.data.capacity,
-                ecu_id : returndata.data.ecu[0],
-                api_Key: apiKey,
-                api_Secret: apiSecret
-        },
-      } 
-    ];
-this.log(device);
-return device;
-    });
+      this.homey.settings.set("sid", sid);
 
+      console.log('Pairing...')
+      })
 
+      session.setHandler("list_devices", async () => 
+      {
+      this.homey.log("Listing devices");
+      var sid='';
+      var apiKey='';
+      var apiSecret='';
+
+      sid = this.homey.settings.get("sid");
+      apiKey =  this.homey.settings.get("apiKey");
+      apiSecret = this.homey.settings.get("apiSecret");
+      //console.log(sid,' ', apiKey,' ', apiSecret);
+
+      const pairApi = new MyApi;
+      const returndata = await pairApi.fetchData('/user/api/v2/systems/details/' + sid,''
+                                        , 'GET', apiKey, apiSecret);
+      devices = 
+        {
+                name: 'APsystems',
+        data : {sid},
+        store: {installdate:  returndata.data.create_date,
+                capacity:     returndata.data.capacity,
+                ecu_id :      returndata.data.ecu[0]} 
+        }
+      
+        this.homey.settings.set("eid", String(devices.store.ecu_id));
+        console.log(devices);
+        return [devices]
+      }
+     )        
+    }
   }
-
-  /*
-  async onPairListDevices() {
-    this.log('Pairing...')
-    const pairApi = new MyApi;
-    
-    const returndata = await pairApi.fetchData('/user/api/v2/systems/details/' + sid,'', 'GET')
-    const device = [
-           { 
-        name: 'APsystems',
-        data : {sid_id: returndata.data.sid},
-        store: {
-                installdate: returndata.data.create_date,
-                capacity: returndata.data.capacity,
-                ecu_id : returndata.data.ecu[0],
-                api_Key: apiKey,
-                api_Secret: apiSecret
-        },
-      } 
-    ];
-this.log(device);
-return device;
-  }
-*/
-};
